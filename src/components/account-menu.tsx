7df1058d6a8de35072f9bc98b9fcdf18c13559bd
nelsoncastanha <dorto@live.com>
2025-03-18 23:58:33 -0300

    dropdown to be fixed

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,37 @@
+import { ChevronDown } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          Pizza Shop
+          <ChevronDown />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Nelson Castanha</span>
+          <span className="text-muted-foreground text-xs font-normal">
+            teste@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem></DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
