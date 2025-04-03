c119c475f31c058a38992812dc74c537747d49be
nelsoncastanha <dorto@live.com>
2025-04-03 11:11:05 -0300

    removed intentional error for testing out error page

--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -7,8 +7,6 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { MetricCardSkeleton } from './metric-card-skeleton'
 
 export function MonthRevenueCard() {
-  throw new Error('Erro no card!')
-
   const { data: monthRevenue } = useQuery({
     queryFn: getMonthRevenue,
     queryKey: ['metrics', 'month-revenue'],
