fdb76a28b25f791fde32128834bc8bf12dd6a43d
nelsoncastanha <dorto@live.com>
2025-05-05 19:04:32 -0300

    e2e sign up

---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,57 @@
+import { expect, test } from '@playwright/test'
+
+test('sign up successfully', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page
+    .getByRole('textbox', { name: 'Nome do estabelecimento' })
+    .fill('Pizza Shop')
+  await page.getByRole('textbox', { name: 'Seu nome' }).fill('John Doe')
+  await page
+    .getByRole('textbox', { name: 'Seu e-mail' })
+    .fill('johndoe@example.com')
+  await page.getByRole('textbox', { name: 'Seu celular' }).fill('123812641264')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Restaurante cadastrado com sucesso')
+
+  expect(toast).toBeVisible()
+
+  await page.waitForTimeout(2000)
+})
+
+test('sign up with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page
+    .getByRole('textbox', { name: 'Nome do estabelecimento' })
+    .fill('Invalid Name')
+  await page.getByRole('textbox', { name: 'Seu nome' }).fill('Fulano Ciclano')
+  await page
+    .getByRole('textbox', { name: 'Seu e-mail' })
+    .fill('aasdsasd@asdae.com')
+  await page.getByRole('textbox', { name: 'Seu celular' }).fill('142641264')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Erro ao cadastrar restaurante.')
+
+  expect(toast).toBeVisible()
+
+  await page.waitForTimeout(2000)
+
+  // const toast = page.getByText('Credenciais inválidas.')
+
+  // expect(toast).toBeVisible()
+
+  // await page.waitForTimeout(2000)
+})
+
+test('navigate to new login page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+
+  expect(page.url()).toContain('/sign-in')
+})
